1c99aebcd9ffe9efd40106ab0117adb986f1406d
Vítězslav Zrůcký <software@zrucky.net>
2018-06-11 01:38:11 +0200

    all

--- Madeta/Paletizace tray/Panel/Panel/MainWindow.xaml.cs
+++ Madeta/Paletizace tray/Panel/Panel/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,6 +23,52 @@ namespace Panel
         public MainWindow()
         {
             InitializeComponent();
+
+            this.SourceInitialized += MainWindow_SourceInitialized;
+
+
+            this.Width = System.Windows.SystemParameters.WorkArea.Width;
+            this.Height = System.Windows.SystemParameters.WorkArea.Height/2;
+            this.Left = 0;
+            this.Top = System.Windows.SystemParameters.WorkArea.Height / 2;
+
+          
+
+        }
+
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            HwndSource source = HwndSource.FromHwnd(helper.Handle);
+            source.AddHook(WndProc);
+        }
+
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+          //  this.Left = 0;
+          //  this.Top = System.Windows.SystemParameters.WorkArea.Height / 2;
+        }
+
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MOVE = 0xF010;
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+
+            switch (msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = wParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                    {
+                        handled = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
         }
     }
 }
